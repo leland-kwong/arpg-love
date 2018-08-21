feaa34dae7753df5065165dd50e8cbc84c83b34f
leland-kwong <leland.kwong@gmail.com>
2018-08-20 22:33:29 -0700

    Update: increase map size

--- src/scene/scene-main.lua
+++ src/scene/scene-main.lua
@@ -73,7 +73,7 @@ insertTestItems(rootState)
 function MainScene.init(self)
   local parent = self
 
-  local map = Map.createAdjacentRooms(4, 20)
+  local map = Map.createAdjacentRooms(6, 25)
   local gridTileDefinitions = cloneGrid(map.grid, function(v, x, y)
     local tileGroup = gridTileTypes[v]
     return tileGroup[math.random(1, #tileGroup)]
