c20560c27069d458cda3a5b3eafc790786f105be
leland-kwong <leland.kwong@gmail.com>
2018-08-21 23:19:37 -0700

    New lime color

--- src/modules/color.lua
+++ src/modules/color.lua
@@ -6,6 +6,7 @@ local Color = {
   SKY_BLUE = {0.8,1,1,1},
   LIME = {0,1,0},
   WHITE = {1,1,1,1},
+  LIME = {rgba255(35, 219, 93)},
   LIGHT_GRAY = {0.7,0.7,0.7,1},
   MED_GRAY = {0.5,0.5,0.5},
   MED_DARK_GRAY = {0.3,0.3,0.3},
