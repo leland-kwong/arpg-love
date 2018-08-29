f59f41292cb6cb3187c0e7086a2c1a9659ea680b
leland-kwong <leland.kwong@gmail.com>
2018-08-28 17:45:27 -0700

    Update children list to be a key-value hash.
    This is necessary for us to be able to quickly remove the child from its
    previous parent if a new parent is set

--- src/modules/component.lua
+++ src/modules/component.lua
@@ -12,24 +12,6 @@ local collisionObject = require 'modules.collision'
 local M = {}
 local allComponentsById = {}
 
--- we want to reuse ids when possible. This makes it easier for us to do things
--- like table pooling because the pool will have a cached object by id readily available.
-local reusableIds = {
-  get = function(self)
-    local idFromList = self[#self]
-    if not idFromList then
-      return uid()
-    end
-    -- remove id from list
-    self[#self] = nil
-    return idFromList
-  end,
-  add = function(self, id)
-    self[#self + 1] = id
-    return id
-  end
-}
-
 -- built-in defaults
 local floor = math.floor
 local baseProps = {
@@ -100,7 +82,7 @@ function M.createFactory(blueprint)
       invalidPropsErrorMsg
     )
 
-    local id = blueprint.id or reusableIds:get()
+    local id = blueprint.id or uid()
     c._id = id
 
     setmetatable(c, blueprint)
@@ -137,11 +119,23 @@ function M.createFactory(blueprint)
     We don't want an `addChild` method so we can avoid coupling between child and parent.
   ]]
   function blueprint:setParent(parent)
-    self.parent = parent
+    local isSameParent = parent == self.parent
+    if isSameParent then
+      return self
+    end
 
+    local id = self:getId()
+    -- dissasociate itself from previous parent
+    local previousParent = self.parent
+    if (previousParent and previousParent._children) then
+      previousParent._children[id] = nil
+    end
+
+    -- set new parent
+    self.parent = parent
     parent._children = parent._children or {}
     -- add self as child to its parent
-    parent._children[#parent._children + 1] = self
+    parent._children[id] = self
     return self
   end
 
@@ -171,8 +165,8 @@ function M.createFactory(blueprint)
 
     local children = self._children
     if (recursive and children) then
-      for i=1, #children do
-        children[i]:delete(true)
+      for _,child in pairs(children) do
+        child:delete(true)
       end
       self._children = nil
     end
@@ -180,7 +174,6 @@ function M.createFactory(blueprint)
     self.group.delete(self)
     cleanupCollisionObjects(self)
     self._deleted = true
-    reusableIds:add(self._id)
     return self
   end
 
