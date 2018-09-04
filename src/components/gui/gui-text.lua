d6ea3801a34990c962365f70ebb44df07cb086ae
leland-kwong <leland.kwong@gmail.com>
2018-09-03 23:36:41 -0700

    Fix memory leak due to incorrect cache variable

--- src/components/gui/gui-text.lua
+++ src/components/gui/gui-text.lua
@@ -6,7 +6,7 @@ local f = require 'utils.functional'
 local textForMeasuringCache = {}
 local function getTextForMeasuring(font)
   local textObject = textForMeasuringCache[font]
-  if not fromCache then
+  if (not textObject) then
     textObject = love.graphics.newText(font, '')
     textForMeasuringCache[font] = textObject
   end
