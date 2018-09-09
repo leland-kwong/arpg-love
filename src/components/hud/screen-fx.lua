6604570e9b8bad46853749d90f1286f8b802e482
leland-kwong <leland.kwong@gmail.com>
2018-09-08 17:57:03 -0700

    Change damage flash to use a vignette

--- src/components/hud/screen-fx.lua
+++ src/components/hud/screen-fx.lua
@@ -4,27 +4,43 @@ local Color = require 'modules.color'
 local screenScale = require 'config.config'.scaleFactor
 local tween = require 'modules.tween'
 local msgBus = require 'components.msg-bus'
+local moonshine = require 'modules.moonshine'
+
+local vignette = love.graphics.newImage('built/images/vignette2.png')
 
 local ScreenFx = {
-  group = groups.gui,
-  opacity = 0
+  group = groups.hud,
+  damageFlashOpacity = 0.3,
+  opacity = 0,
 }
 
 -- flashes the screen red when the player gets hit
 
+local function getScreenSize()
+  return love.graphics.getWidth() / screenScale,
+    love.graphics.getHeight() / screenScale
+end
+
 function ScreenFx.init(self)
-  self.tween = tween.new(2, self, {opacity = 0}, tween.easing.outExpo)
+  self.tween = tween.new(1.8, self, {opacity = 0}, tween.easing.outExpo)
   msgBus.subscribe(function(msgType, msgValue)
     if self:isDeleted() then
       return msgBus.CLEANUP
     end
 
     if msgBus.PLAYER_HIT_RECEIVED == msgType then
-      self.opacity = 0.7
+      self.opacity = self.damageFlashOpacity
       self.tween:reset()
       self.currentTween = self.tween
     end
   end)
+
+  local screenWidth, screenHeight = getScreenSize()
+  self.glowEffect = moonshine(screenWidth, screenHeight, moonshine.effects.vignette)
+  self.glowEffect.vignette.radius = 0.1
+  self.glowEffect.vignette.softness = 0.1
+  self.glowEffect.vignette.opacity = 0.9
+  self.glowEffect.vignette.color = {255,0,0}
 end
 
 -- we do a two-step tween to animate in then out
@@ -39,18 +55,11 @@ end
 
 function ScreenFx.draw(self)
   if self.currentTween then
-    local originalLineWidth = love.graphics.getLineWidth()
-    love.graphics.setColor(Color.rgba255(242, 51, 26, self.opacity))
-    local thickness = 20
-    love.graphics.setLineWidth(thickness)
-    love.graphics.rectangle(
-      'line',
-      0, 0,
-      love.graphics.getWidth() / screenScale,
-      love.graphics.getHeight() / screenScale
+    love.graphics.setColor(1,0,0,self.opacity)
+    love.graphics.draw(
+      vignette,
+      0, 0
     )
-    -- reset line width
-    love.graphics.setLineWidth(originalLineWidth)
   end
 end
 
