d4dcb5ad7d9f9f651c56628e30c0a7b9cc632ea1
leland-kwong <leland.kwong@gmail.com>
2018-10-07 16:31:03 -0700

    Update tooltip test

--- src/scene/tooltip-test/init.lua
+++ src/scene/tooltip-test/init.lua
@@ -4,16 +4,17 @@ local msgBusMainMenu = require 'components.msg-bus-main-menu'
 local Block = require 'components.gui.block'
 local Color = require 'modules.color'
 local font = require 'components.font'
+local modParser = require 'modules.loot.item-modifier-template-parser'
 
 local TooltipTest = {}
 
-function TooltipTest.init()
+function TooltipTest.init(self)
   msgBus.send(msgBus.SET_BACKGROUND_COLOR, Color.LIGHT_GRAY)
   local blockPadding = 8
 
   local titleBlock = {
     content = {
-      Color.YELLOW,
+      Color.RARITY_MAGICAL,
       'Fast Pod Initate of the Bear'
     },
     width = 125,
@@ -21,7 +22,7 @@ function TooltipTest.init()
     font = font.secondary.font,
     fontSize = font.secondary.fontSize,
   }
-  local levelBlock = {
+  local levelRequiredBlock = {
     content = {
       Color.WHITE,
       'Level 1'
@@ -33,36 +34,28 @@ function TooltipTest.init()
   }
   local itemTypeBlock = {
     content = {
-      Color.YELLOW,
-      'Rare Pod Module'
+      Color.RARITY_MAGICAL,
+      'Pod Module'
     },
     width = 125,
     font = font.primary.font,
     fontSize = font.primary.fontSize
   }
   local upgradeModifierBlock = {
-    content = {
-      Color.YELLOW,
-      'Critical Strikes\n',
-
-      Color.LIME,
-      '20 experience to unlock\n',
-
-      Color.OFF_WHITE,
-      '\nAttacks have a ',
-
-      Color.WHITE,
-      '25% ',
-
-      Color.OFF_WHITE,
-      'chance to deal ',
-
-      Color.WHITE,
-      '1.2x - 1.4x ',
-
-      Color.OFF_WHITE,
-      'damage'
-    },
+    content = modParser({
+      type = 'upgrade',
+      data = {
+        title = 'Critical Strikes',
+        experienceRequired = 20,
+        description = {
+          template = '{chance} chance to deal {dmgMultiplier} damage.',
+          data = {
+            chance = '25%',
+            dmgMultiplier = '1.2x - 1.4x'
+          }
+        }
+      }
+    }),
     width = 250,
     font = font.primary.font,
     fontSize = font.primary.fontSize,
@@ -70,21 +63,27 @@ function TooltipTest.init()
     padding = blockPadding
   }
   local baseStatsBlock = {
-    content = {
-      Color.OFF_WHITE,
-      '+',
-      Color.WHITE,
-      5,
-      Color.OFF_WHITE,
-      ' Lightning Damage',
-
-      Color.OFF_WHITE,
-      '\n+',
-      Color.WHITE,
-      tostring(12)..'%',
-      Color.OFF_WHITE,
-      ' Cooldown Reduction\n',
-    },
+    content = modParser({
+      type = 'statsList',
+      data = {
+        lightningDamage = 2,
+        cooldownReduction = 5,
+      }
+    }),
+    width = 250,
+    font = font.primary.font,
+    fontSize = font.primary.fontSize,
+    background = {0.3, 0.3, 0.3},
+    padding = blockPadding,
+  }
+  local baseStatsBlock2 = {
+    content = modParser({
+      type = 'statsList',
+      data = {
+        lightningDamage = 2,
+        cooldownReduction = 5,
+      }
+    }),
     width = 250,
     font = font.primary.font,
     fontSize = font.primary.fontSize,
@@ -94,23 +93,30 @@ function TooltipTest.init()
   local rows = {
     Block.Row({
       titleBlock,
-      levelBlock
+      levelRequiredBlock
+    }, {
+      marginBottom = 8
     }),
     Block.Row({
       itemTypeBlock
     }, {
-      marginTop = 8
+      marginBottom = 8
     }),
     Block.Row({
       baseStatsBlock
     }, {
-      marginTop = 8
+      marginTop = 1
     }),
     Block.Row({
       upgradeModifierBlock
     }, {
       marginTop = 1
-    })
+    }),
+    Block.Row({
+      baseStatsBlock2
+    }, {
+      marginTop = 1
+    }),
   }
   Block.create({
     x = 250,
@@ -118,7 +124,7 @@ function TooltipTest.init()
     background = {0.2,0.2,0.2},
     padding = blockPadding,
     rows = rows
-  })
+  }):setParent(self)
 end
 
 local Scene = Component.createFactory(TooltipTest)
@@ -126,7 +132,7 @@ local Scene = Component.createFactory(TooltipTest)
 msgBusMainMenu.send(msgBusMainMenu.MENU_ITEM_ADD, {
   name = 'tooltip test',
   value = function()
-    msgBus.send(msgBus.SCENE_STACK_PUSH, {
+    msgBus.send(msgBus.SCENE_STACK_REPLACE, {
       scene = Scene
     })
   end
