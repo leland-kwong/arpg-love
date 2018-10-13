04e3bd1217ee898f6541696ea997ec825f41bb5a
leland-kwong <leland.kwong@gmail.com>
2018-10-13 14:20:32 -0700

    Add new modifiers and balance others

--- src/components/item-inventory/modifier-definitions.lua
+++ src/components/item-inventory/modifier-definitions.lua
@@ -1,3 +1,6 @@
+--[[
+  loot modifier definitions
+]]
 local Vec2 = require 'modules.brinevector'
 
 return {
@@ -5,10 +8,10 @@ return {
     range = Vec2(0.01, 0.1),
   },
   attackTimeReduction = {
-    range = Vec2(0.01, 0.3)
+    range = Vec2(0.01, 0.1)
   },
   percentDamage = {
-    range = Vec2(0.2, 0.5)
+    range = Vec2(0.1, 0.2)
   },
   armor = {
     range = Vec2(10,100)
@@ -16,7 +19,16 @@ return {
   maxHealth = {
     range = Vec2(50, 100)
   },
+  healthRegeneration = {
+    range = Vec2(3, 6)
+  },
   maxEnergy = {
     range = Vec2(10, 20)
-  }
+  },
+  energyRegeneration = {
+    range = Vec2(1, 2)
+  },
+  moveSpeed = {
+    range = Vec2(10, 15)
+  },
 }
