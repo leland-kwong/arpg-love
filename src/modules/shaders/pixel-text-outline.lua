605753180f325cae1b80b20e292e72c548fb1149
leland-kwong <leland.kwong@gmail.com>
2018-10-13 00:38:41 -0700

    Add support for more options

--- src/modules/shaders/pixel-text-outline.lua
+++ src/modules/shaders/pixel-text-outline.lua
@@ -2,14 +2,15 @@ local shaderSource = love.filesystem.read('modules/shaders/pixel-outline.fsh')
 local defaultOutlineColor = {0,0,0,1}
 local shader = love.graphics.newShader(shaderSource)
 local textW, textH = 16, 16
-local spriteSize = {textW, textH}
+local defaultSpriteSize = {textW, textH}
 
 local PixelTextShader = {}
 
-function PixelTextShader.attach(outlineColor, alpha)
+function PixelTextShader.attach(outlineColor, alpha, outlineWidth, spriteSize)
+  spriteSize = spriteSize or defaultSpriteSize
   love.graphics.setShader(shader)
-  shader:send('sprite_size', spriteSize)
-  shader:send('outline_width', 2/textW)
+  shader:send('sprite_size', spriteSize or defaultSpriteSize)
+  shader:send('outline_width', (outlineWidth or 2)/spriteSize[1])
   shader:send('outline_color', outlineColor or defaultOutlineColor)
   shader:send('use_drawing_color', true)
   shader:send('include_corners', true)
