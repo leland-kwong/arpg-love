2146f37310daa18c2edca619f7e6e28cb3a5dded
leland-kwong <leland.kwong@gmail.com>
2018-10-14 23:46:27 -0700

    default alpha to 1

--- src/modules/color.lua
+++ src/modules/color.lua
@@ -12,13 +12,13 @@ function Color.multiply(a, b)
     return a[1],
       a[2],
       a[3],
-      a[4]
+      (a[4] or 1)
   end
   return
     a[1] * b[1],
     a[2] * b[2],
     a[3] * b[3],
-    a[4] * b[4]
+    (a[4] or 1) * (b[4] or 1)
 end
 
 local alphaColor = {1,1,1,1}
