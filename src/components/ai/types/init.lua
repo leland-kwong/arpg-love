351d111c6777c87ed78cb245beebc2500cd5284c
leland-kwong <leland.kwong@gmail.com>
2018-10-26 15:52:42 -0700

    Re enable all enemy types

--- src/components/ai/types/init.lua
+++ src/components/ai/types/init.lua
@@ -2,16 +2,16 @@ local aiTypesPath = 'components.ai.types.'
 local Enum = require 'utils.enum'
 
 local aiType = Enum({
-  -- 'SLIME',
-  -- 'MINI_BOT',
-  -- 'EYEBALL',
+  'SLIME',
+  'MINI_BOT',
+  'EYEBALL',
   'MELEE_BOT'
 })
 
 local aiTypeDef = {
-  -- [aiType.SLIME] =      require(aiTypesPath..'ai-slime'),
-  -- [aiType.MINI_BOT] =   require(aiTypesPath..'ai-mini-bot'),
-  -- [aiType.EYEBALL] =    require(aiTypesPath..'ai-eyeball'),
+  [aiType.SLIME] =      require(aiTypesPath..'ai-slime'),
+  [aiType.MINI_BOT] =   require(aiTypesPath..'ai-mini-bot'),
+  [aiType.EYEBALL] =    require(aiTypesPath..'ai-eyeball'),
   [aiType.MELEE_BOT] =    require(aiTypesPath..'ai-melee-bot')
 }
 
