2784d6c0f22612e4a6b335280c8a9638b8c92bd8
leland-kwong <leland.kwong@gmail.com>
2018-10-28 18:18:51 -0700

    Use a single emitter for sparks

--- src/components/abilities/spark.lua
+++ src/components/abilities/spark.lua
@@ -8,27 +8,52 @@ local Color = require 'modules.color'
 
 local Spark = {
   color = Color.WHITE,
-  maxLifeTime = 0.35,
+  maxLifeTime = 0.30,
   opacity = 1
 }
 
 local animation = AnimationFactory:newStaticSprite('pixel-white-1x1')
 
+local psystem = love.graphics.newParticleSystem(AnimationFactory.atlas, 500)
+psystem:setQuads(animation.sprite)
+psystem:setOffset(animation:getOffset())
+-- spread 360 degrees
+psystem:setSpread(-math.pi * 2, math.pi * 2)
+local speed = 50
+psystem:setSpeed(-speed, speed)
+local acceleration = 100
+psystem:setLinearAcceleration(-acceleration, -acceleration, acceleration, acceleration) -- move particles in all random directions
+psystem:setSizes(1, 1, 1, 2)
+psystem:setSizeVariation(1)
+psystem:setEmissionArea('ellipse', 5, 5, 0, true)
+
+-- particle system
+local ParticleSystem = {}
+
+function ParticleSystem.init(self)
+  Component.addToGroup(self, 'all')
+end
+
+function ParticleSystem.update(self, dt)
+  psystem:update(dt)
+end
+
+function ParticleSystem.draw(self)
+  love.graphics.draw(psystem, 0, 0)
+end
+
+function ParticleSystem.drawOrder()
+  local drawOrders = require 'modules.draw-orders'
+  return drawOrders.SparkDraw
+end
+
+Component.create(ParticleSystem)
+
 function Spark.init(self)
   Component.addToGroup(self, 'all')
   Component.addToGroup(self, 'gameWorld')
 
-  local psystem = love.graphics.newParticleSystem(AnimationFactory.atlas, 20)
-  psystem:setQuads(animation.sprite)
-  psystem:setOffset(animation:getOffset())
   psystem:setParticleLifetime(0.25, self.maxLifeTime)
-  psystem:setEmissionRate(50)
-  psystem:setSpeed(30)
-  psystem:setRotation(0, math.pi/2)
-  psystem:setSizes(1, 4)
-  psystem:setSizeVariation(1)
-  -- psystem:setLinearAcceleration(0, 0, 0, 0)
-  psystem:setEmissionArea('ellipse', 10, 10, 0, false)
   local col = self.color
   psystem:setColors(
     col[1], col[2], col[3], 0.1,
@@ -36,44 +61,21 @@ function Spark.init(self)
     1, 1, 1, 0.75,
     1, 1, 1, 0
   )
-  self.psystem = psystem
+  psystem:setPosition(self.x, self.y)
+  psystem:emit(30)
+
   self.clock = 0
-
-  local dt = 1/60
-  -- setup particles in all random directions
-  for i=1, 30 do
-    self.psystem:update(dt)
-    self.angle = self.angle + dt * 20
-    self.psystem:setDirection(self.angle)
-  end
-  self.psystem:setEmissionRate(0)
-  self.psystem:setDirection(-math.pi/4)
-  self.lightColor = {1,1,1,1}
-
   local numFrames = self.maxLifeTime / dt
   self.opacityPerFrame = 1/numFrames
 end
 
 function Spark.update(self, dt)
-  self.psystem:update(dt)
-
   self.clock = self.clock + dt
   self.opacity = self.opacity - self.opacityPerFrame
   if self.clock >= self.maxLifeTime then
     self:delete(true)
   end
-  self.lightColor[4] = self.lightColor[4] * self.opacity
-  Component.get('lightWorld'):addLight(self.x, self.y, 20, self.lightColor)
-end
-
-function Spark.draw(self)
-  -- love.graphics.setColor(1,1,1,self.opacity)
-  love.graphics.draw(self.psystem, self.x, self.y)
-end
-
-function Spark.drawOrder()
-  local drawOrders = require 'modules.draw-orders'
-  return drawOrders.SparkDraw
+  Component.get('lightWorld'):addLight(self.x, self.y, 20, nil, self.opacity)
 end
 
 return Component.createFactory(Spark)
