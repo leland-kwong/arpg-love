0d989fbd4ea9f4a259e41361acff51ce509119ba
leland-kwong <leland.kwong@gmail.com>
2018-11-01 00:08:02 -0700

    Modularize player code

--- src/components/player/init.lua
+++ src/components/player/init.lua
@@ -76,67 +76,9 @@ local function connectInventory()
 
   -- add default weapons
   if rootState:get().isNewGame then
-    local items = {
-      {
-        type = 'base.potion-health',
-        position = {
-          x = 1,
-          y = 5
-        }
-      },
-      {
-        type = 'base.pod-module-initiate',
-        position = {
-          x = 1,
-          y = 1
-        }
-      },
-      {
-        type = 'base.potion-energy',
-        position = {
-          x = 2,
-          y = 5
-        }
-      },
-      {
-        type = 'base.mock-shoes',
-        position = {
-          x = 1,
-          y = 4
-        }
-      },
-      {
-        type = 'base.pod-module-hammer',
-      },
-      {
-        type = 'legendary.augmentation-module-frenzy',
-      },
-      {
-        type = 'base.augmentation-module-one',
-        position = {
-          x = 2,
-          y = 4
-        }
-      },
-      -- {
-      --   type = 'lightning-rod',
-      --   position = {
-      --     x = 1,
-      --     y = 2
-      --   }
-      -- },
-      -- {
-      --   type = 'mock-armor',
-      --   position = {
-      --     x = 2,
-      --     y = 3
-      --   }
-      -- },
-      -- {
-      --   type = 'pod-module-fireball'
-      -- }
-    }
-    setupDefaultInventory(items)
+    setupDefaultInventory(
+      require 'components.player.starting-items'
+    )
   end
 
   -- trigger equipment change for items that were previously equipped from loading the state
---
+++ src/components/player/starting-items.lua
@@ -0,0 +1,63 @@
+return {
+  {
+    type = 'base.potion-health',
+    position = {
+      x = 1,
+      y = 5
+    }
+  },
+  {
+    type = 'base.pod-module-initiate',
+    position = {
+      x = 1,
+      y = 1
+    }
+  },
+  {
+    type = 'base.potion-energy',
+    position = {
+      x = 2,
+      y = 5
+    }
+  },
+  {
+    type = 'base.mock-shoes',
+    position = {
+      x = 1,
+      y = 4
+    }
+  },
+  {
+    type = 'base.pod-module-hammer',
+  },
+  {
+    type = 'legendary.augmentation-module-frenzy',
+  },
+  {
+    type = 'legendary.defender-of-aureus',
+  },
+  {
+    type = 'base.augmentation-module-one',
+    position = {
+      x = 2,
+      y = 4
+    }
+  },
+  -- {
+  --   type = 'lightning-rod',
+  --   position = {
+  --     x = 1,
+  --     y = 2
+  --   }
+  -- },
+  -- {
+  --   type = 'mock-armor',
+  --   position = {
+  --     x = 2,
+  --     y = 3
+  --   }
+  -- },
+  -- {
+  --   type = 'pod-module-fireball'
+  -- }
+}
