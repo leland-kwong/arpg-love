cf981dcfcb6c39cff67356fb5ead249cb14d562d
leland-kwong <leland.kwong@gmail.com>
2018-12-08 16:48:36 -0800

    remove global

--- src/utils/pixels-to-grid-units.lua
+++ src/utils/pixels-to-grid-units.lua
@@ -1,11 +1,10 @@
-local Math = require("utils.math")
-local round = Math.round
+local floor = math.floor
 
 -- returns coordinate values in tile units centered to the tile
 local function pixelsToGridUnits(screenX, screenY, gridSize)
 	local gridX, gridY =
-		math.floor(screenX / gridSize),
-		math.floor(screenY / gridSize)
+		floor(screenX / gridSize),
+		floor(screenY / gridSize)
 	return gridX, gridY
 end
 
