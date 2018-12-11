56f8f77f820b61baa04c14a187af215e6d5a6178
leland-kwong <leland.kwong@gmail.com>
2018-12-10 18:02:59 -0800

    Remove legacy comments

--- src/scripts/aseprite-split-slices.bat
+++ src/scripts/aseprite-split-slices.bat
@@ -1,6 +1,3 @@
-REM copy and paste this into the command line.
-REM The aseprite path should be relative to the folder you're running this command in
-
 SET ProjectDir=C:\Users\lelandkwong\Projects\arpg-love
 SET SourceDir=%ProjectDir%\assets\sprites\custom-art
 SET AsepritePath="C:\Program Files\Aseprite\Aseprite.exe"
