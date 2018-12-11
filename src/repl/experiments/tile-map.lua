bc7720926009e0c533e4576abcaac432fb0813e5
leland-kwong <leland.kwong@gmail.com>
2018-12-10 17:01:43 -0800

    Add crosshair for center of screen

--- src/repl/experiments/tile-map.lua
+++ src/repl/experiments/tile-map.lua
@@ -8,7 +8,6 @@ end
 local tileBitmasking = dynamicLoad 'utils.tilemap-bitmask'
 local Grid = dynamicLoad 'utils.grid'
 local Camera = dynamicLoad 'modules.camera'
-local camera = Camera()
 local msgBus = require 'components.msg-bus'
 
 local json = dynamicLoad 'lua_modules.json'
@@ -62,16 +61,44 @@ state = state or {
   }
 }
 
+local camera = Camera()
+
 local newGrid = {}
 tileBitmasking.iterateGrid(grid, function(v, x, y)
   Grid.set(newGrid, x, y, v)
 end, 0)
 
+local function drawScreenCenter()
+  love.graphics.push()
+  love.graphics.origin()
+  local x, y = love.graphics.getDimensions()
+  x, y = x/2, y/2
+
+  local crosshairSize = 10
+  love.graphics.setColor(1,1,1)
+  local oLineWidth = love.graphics.getLineWidth()
+  love.graphics.setLineWidth(2)
+  love.graphics.line(
+    x - crosshairSize/2,
+    y,
+    x + crosshairSize/2,
+    y
+  )
+  love.graphics.line(
+    x,
+    y - crosshairSize/2,
+    x,
+    y + crosshairSize/2
+  )
+  love.graphics.setLineWidth(oLineWidth)
+  love.graphics.pop()
+end
+
 local Component = require 'modules.component'
 Component.create({
   id = 'tile-map-test',
   init = function(self)
-    -- Component.addToGroup(self, 'gui')
+    Component.addToGroup(self, 'gui')
 
     local InputContext = dynamicLoad 'modules.input-context'
     InputContext.set('Tilemap-test')
@@ -142,6 +169,7 @@ Component.create({
     Grid.forEach(newGrid, drawTileCap)
 
     camera:detach()
+    drawScreenCenter()
   end,
 
   drawOrder = function()
