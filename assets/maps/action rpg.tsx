b6d53077cf3e21bfe6cc83d4035af3fe0e0da485
leland-kwong <leland.kwong@gmail.com>
2018-12-12 22:54:25 -0800

    remove old assets

--- assets/maps/action rpg.tsx
+++ assets/maps/action rpg.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="action rpg" tilewidth="34" tileheight="56" tilecount="17" columns="0">
+<tileset version="1.2" tiledversion="1.2.1" name="action rpg" tilewidth="36" tileheight="28" tilecount="11" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="16" height="16" source="../sprites/custom-art/tiles/floor-1.png"/>
@@ -12,24 +12,6 @@
  </tile>
  <tile id="3">
   <image width="16" height="16" source="../sprites/custom-art/tiles/floor-4.png"/>
- </tile>
- <tile id="4">
-  <image width="16" height="56" source="../sprites/custom-art/tiles/wall-1.png"/>
- </tile>
- <tile id="5">
-  <image width="16" height="56" source="../sprites/custom-art/tiles/wall-2.png"/>
- </tile>
- <tile id="6">
-  <image width="16" height="56" source="../sprites/custom-art/tiles/wall-3.png"/>
- </tile>
- <tile id="7">
-  <image width="16" height="56" source="../sprites/custom-art/tiles/wall-4.png"/>
- </tile>
- <tile id="8">
-  <image width="16" height="56" source="../sprites/custom-art/tiles/wall-5.png"/>
- </tile>
- <tile id="9">
-  <image width="16" height="56" source="../sprites/custom-art/tiles/wall-6.png"/>
  </tile>
  <tile id="10">
   <image width="16" height="16" source="../sprites/custom-art/tiles/floor-5.png"/>
@@ -50,6 +32,6 @@
   <image width="16" height="16" source="../sprites/custom-art/tiles/floor-10.png"/>
  </tile>
  <tile id="22">
-  <image width="34" height="28" source="../sprites/custom-art/tiles/treasure-chest.png"/>
+  <image width="36" height="28" source="../sprites/custom-art/tiles/treasure-chest.png"/>
  </tile>
 </tileset>
