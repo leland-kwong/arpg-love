b598b10590beb4bb40cad0d41880597c88b2404f
leland-kwong <leland.kwong@gmail.com>
2018-12-13 21:32:17 -0800

    Update vignette size to match the scale

--- src/components/hud/screen-fx.lua
+++ src/components/hud/screen-fx.lua
@@ -8,7 +8,7 @@ local vignette = love.graphics.newImage('built/images/vignette2.png')
 
 local ScreenFx = {
   group = groups.hud,
-  damageFlashOpacity = 0.3,
+  damageFlashOpacity = 0.5,
   opacity = 0,
 }
 
@@ -36,6 +36,7 @@ end
 
 -- we do a two-step tween to animate in then out
 function ScreenFx.update(self, dt)
+
   if self.currentTween then
     local complete = self.currentTween:update(dt)
     if complete then
@@ -46,11 +47,21 @@ end
 
 function ScreenFx.draw(self)
   if self.currentTween then
+    local camera = require 'components.camera'
+    local cameraWidth = camera:getSize() * camera.scale
+    local scale = cameraWidth / vignette:getPixelWidth()
+
+    love.graphics.push()
+    love.graphics.origin()
+    love.graphics.scale(scale)
+
     love.graphics.setColor(1,0,0,self.opacity)
     love.graphics.draw(
       vignette,
       0, 0
     )
+
+    love.graphics.pop()
   end
 end
 
