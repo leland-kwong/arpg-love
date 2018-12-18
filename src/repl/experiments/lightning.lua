1702c9823cba9318ea965d4acc4a0b2faeda9767
leland-kwong <leland.kwong@gmail.com>
2018-12-17 20:47:43 -0800

    add support for duration and dynamic adding

--- src/repl/experiments/lightning.lua
+++ src/repl/experiments/lightning.lua
@@ -9,19 +9,19 @@ local glowEffect = moonshine(
   moonshine.effects.glow
 )
 
-glowEffect.glow.strength = 4
-glowEffect.glow.min_luma = 0.4
+glowEffect.glow.strength = 5
+glowEffect.glow.min_luma = 0.0
 
 local options = {
   minDeviation = 10,
   maxDeviation = 25
 }
 
-local function generateLightning(startPt, endPt)
+local function generateLightning(startPt, target)
   local Position = require 'utils.position'
   local Math = require 'utils.math'
 
-  local x1, y1, x2, y2 = startPt.x, startPt.y, endPt.x, endPt.y
+  local x1, y1, x2, y2 = startPt.x, startPt.y, target.x, target.y
   local totalDist = Math.dist(x1, y1, x2, y2)
   local dx, dy = Position.getDirection(x1, y1, x2, y2)
   local firstSegmentDist = math.min(totalDist, math.random(5, 18))
@@ -47,7 +47,8 @@ local function generateLightning(startPt, endPt)
 
   local function addVertice(dist)
     local length = math.random(options.minDeviation, options.maxDeviation)
-    dirIndex = math.random(1, #directions)
+    dirIndex = dirIndex == 1 and 2 or 1
+    -- dirIndex = math.random(1, #directions)
     local dir = directions[dirIndex]
     local startX, startY = x1 + dx * dist, y1 + dy * dist
     local x, y = startX + dir[1] * length, startY + dir[2] * length
@@ -85,12 +86,24 @@ local function lightSource(x, y, radius)
   love.graphics.draw(lightBlur, x - radius, y - radius, 0, scale, scale)
 end
 
+local addParamsMt = {
+  start = Vec2(0,0),
+  target = Vec2(10, 10),
+  duration = 0.4,
+  opacity = 1,
+
+  color = {0.4, 0.8, 1},
+  -- color = {1, 0.7, 0},
+
+  thickness = 2,
+}
+addParamsMt.__index = addParamsMt
+local instanceTweenTarget = {opacity = 0}
+
 Component.create({
   id = 'lightning-generator-test',
   x = 350,
   y = 250,
-  baseColor = {1.0, 1.0, 1.0},
-  lightColor = {1, 0.7, 0},
   target = Vec2(700, 400),
   init = function(self)
     Component.addToGroup(self, 'gui')
@@ -98,8 +111,13 @@ Component.create({
 
     self.clock = 0
     self.listeners = {
-      msgBus.on(msgBus.MOUSE_DRAG, function(ev)
-        self.target = Vec2(ev.x, ev.y)
+      msgBus.on(msgBus.MOUSE_CLICKED, function(ev)
+        local x, y = unpack(ev)
+        self:add({
+          start = Vec2(self.x, self.y),
+          target = Vec2(x, y),
+          thickness = 2
+        })
       end)
     }
     self.stencil = function()
@@ -109,29 +127,34 @@ Component.create({
       end
     end
     self.canvas = love.graphics.newCanvas()
+    self.sources = {}
+  end,
+
+  add = function(self, params)
+    local instance = setmetatable(params, addParamsMt)
+    instance.vertices = generateLightning(
+      params.start,
+      params.target
+    )
+    local tween = require 'modules.tween'
+    instance.tween = tween.new(params.duration, instance, instanceTweenTarget, tween.easing.inQuad)
+    table.insert(
+      self.sources,
+      instance
+    )
+    return self
   end,
 
   update = function(self, dt)
-    self.clock = self.clock + dt
-    local interval = 0.05
-    if (self.clock > interval and self.target) then
-      self.clock = 0
-      local startPoints = {
-        Vec2(600, 200),
-        Vec2(650, 200),
-        Vec2(500, 300),
-      }
-      local f = require 'utils.functional'
-      self.sources = f.map(startPoints, function(start)
-        return {
-          start = start,
-          endPt = self.target,
-          vertices = generateLightning(
-            start,
-            self.target
-          )
-        }
-      end)
+    local i = 1
+    while i <= #self.sources do
+      local s = self.sources[i]
+      local complete = s.tween:update(dt)
+      if complete then
+        table.remove(self.sources, i)
+      else
+        i = i + 1
+      end
     end
   end,
 
@@ -139,40 +162,41 @@ Component.create({
     local bgColor = {0.2,0.2,0.2}
     love.graphics.clear(bgColor)
 
-    love.graphics.push()
-    love.graphics.origin()
-    love.graphics.setCanvas{self.canvas, stencil=true}
-    love.graphics.clear()
+    if #self.sources > 0 then
+      love.graphics.push()
+      love.graphics.origin()
+      love.graphics.setCanvas{self.canvas, stencil=true}
+      love.graphics.clear()
 
-    local oBlendMode = love.graphics.getBlendMode()
-    local Color = require 'modules.color'
-    if self.sources then
+      local oBlendMode = love.graphics.getBlendMode()
+      local Color = require 'modules.color'
+
       -- draw base lines
       for i=1, #self.sources do
         local source = self.sources[i]
         love.graphics.setLineStyle('rough')
 
-        love.graphics.setLineWidth(4)
+        love.graphics.setLineWidth(source.thickness)
         love.graphics.setBlendMode('alpha')
-        love.graphics.setColor(Color.multiplyAlpha(self.baseColor, 0.4))
+        love.graphics.setColor(Color.multiplyAlpha(source.color, 0.35 * source.opacity))
         love.graphics.line(source.vertices)
       end
 
+      love.graphics.setBlendMode('add', 'alphamultiply')
       for i=1, #self.sources do
         local source = self.sources[i]
-        love.graphics.setBlendMode('add', 'alphamultiply')
-        love.graphics.setColor(Color.multiplyAlpha(self.lightColor, 0.6))
+        love.graphics.setColor(Color.multiplyAlpha(source.color, 0.6 * source.opacity))
 
         -- start point light
-        lightSource(source.start.x, source.start.y, math.random(15, 20))
+        lightSource(source.start.x, source.start.y, source.thickness * 8)
 
         -- end point light
         local lastVertice = #source.vertices - 1
-        love.graphics.setColor(Color.multiplyAlpha(self.lightColor, 0.5))
+        love.graphics.setColor(Color.multiplyAlpha(source.color, 0.5 * source.opacity))
         lightSource(
           source.vertices[lastVertice],
           source.vertices[lastVertice + 1],
-          math.random(8, 10)
+          source.thickness * 4
         )
       end
 
@@ -181,30 +205,28 @@ Component.create({
       -- add a gradient effect on points
       love.graphics.stencil(self.stencil, 'replace', 1)
       love.graphics.setStencilTest('greater', 0)
+      love.graphics.setBlendMode('add', 'alphamultiply')
       for i=1, #self.sources do
         local source = self.sources[i]
         local vertices = source.vertices
-        local oBlendMode = love.graphics.getBlendMode()
-        love.graphics.setBlendMode('add', 'alphamultiply')
-        love.graphics.setColor(Color.multiplyAlpha(self.lightColor, 0.9))
+        love.graphics.setColor(Color.multiplyAlpha(source.color, 0.9 * source.opacity))
         for i=3, (#vertices), 2 do
           local x, y = vertices[i], vertices[i + 1]
-          lightSource(x, y, math.random(40, 60))
+          lightSource(x, y, math.random(40, 70))
         end
       end
 
       love.graphics.setStencilTest()
+
+      love.graphics.setCanvas()
+      love.graphics.setColor(1,1,1)
+      love.graphics.setBlendMode('alpha', 'premultiplied')
+      glowEffect(function()
+        love.graphics.draw(self.canvas)
+      end)
+      love.graphics.setBlendMode(oBlendMode)
+      love.graphics.pop()
     end
-
-    love.graphics.setCanvas()
-    love.graphics.setColor(1,1,1)
-    love.graphics.setBlendMode('alpha', 'premultiplied')
-    glowEffect(function()
-      love.graphics.draw(self.canvas)
-    end)
-    love.graphics.setBlendMode(oBlendMode)
-    love.graphics.pop()
-
   end,
 
   drawOrder = function(self)
