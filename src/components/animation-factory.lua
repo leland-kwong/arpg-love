6016764311ae69a4f202283e5f6710758d17ae53
Leland Kwong <leland.kwong@gmail.com>
2018-12-25 22:22:19 -0800

    remove unused object

--- src/components/animation-factory.lua
+++ src/components/animation-factory.lua
@@ -2,7 +2,6 @@ local json = require 'lua_modules.json'
 local Animation = require 'modules.animation'
 
 local spriteAtlas = love.graphics.newImage('built/sprite.png')
-local spriteAtlasLinear = love.graphics.newImage('built/sprite.png')
 local spriteData = json.decode(
   love.filesystem.read('built/sprite.json')
 )
