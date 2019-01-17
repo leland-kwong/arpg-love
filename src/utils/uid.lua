13bb144b2b5940533ea6914c8f39ae2d973eeabc
leland-kwong <leland.kwong@gmail.com>
2019-01-16 18:39:06 -0800

    Remove string concatenationfor huge performance boost

--- src/utils/uid.lua
+++ src/utils/uid.lua
@@ -1,19 +1,8 @@
-local socket = require 'socket'
-math.randomseed(socket.gettime())
-
-local random = math.random
-
-local function toHex(number)
-	return string.format('%x', number)
-end
-
-local seed = toHex(socket.gettime() * 10000)..'_'
 local increment = 0
 local function uid()
-	local newId = seed..increment
 	-- increment
 	increment = increment + 1
-	return newId
+	return tostring(increment)
 end
 
 return uid
