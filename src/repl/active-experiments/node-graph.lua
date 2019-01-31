6b9bfd523ea2121879a85ff3deac9a5058a50773
leland-kwong <leland.kwong@gmail.com>
2019-01-31 00:30:19 -0800

    [node-graph] wip

--- src/repl/active-experiments/node-graph.lua
+++ src/repl/active-experiments/node-graph.lua
@@ -1,14 +1,18 @@
 local dynamicRequire = require 'utils.dynamic-require'
 local Component = require 'modules.component'
 local msgBus = require 'components.msg-bus'
-local Collision = require 'repl.libs.collision'
+local Gui = require 'components.gui.gui'
 local uid = dynamicRequire 'utils.uid'
 local Vec2 = require 'modules.brinevector'
 local ser = require 'utils.ser'
 local Grid = require 'utils.grid'
-local bump = require 'modules.bump'
+local Color = require 'modules.color'
+local Font = require 'components.font'
+local getTextSize = require 'repl.libs.get-text-size'
 
-bump.newWorld(32)
+local state = {
+  distScale = 2
+}
 
 local nodeMt = {
   position = Vec2()
@@ -61,18 +65,50 @@ local Node = {
 }
 
 local graphColors = {
-  default = {0,1,1},
-  secret = {1,1,1,0}
+  region = {
+    aureus = {Color.rgba255(44, 232, 245)},
+    secret = {Color.rgba255(245, 98, 245)},
+    saria = {Color.rgba255(254,174,52)},
+    default = {0.5,0.5,0.5}
+  },
+  default = {Color.rgba255(44, 232, 245)},
+  secret = {1,1,1,0},
+  link = {1,1,1}
 }
 
 local function renderNode(nodeId, distScale)
   love.graphics.setColor(0,1,1)
   local ref = Node:get(nodeId)
   local p = ref.position * distScale
-  love.graphics.setColor(graphColors.default)
-  love.graphics.circle('fill', p.x, p.y, 2)
+  local c = graphColors.region[ref.region] or graphColors.region.default
+  love.graphics.setColor(c)
+  love.graphics.circle('fill', p.x, p.y, 3)
 
-  love.graphics.circle('line', p.x, p.y, 8)
+  love.graphics.setColor(Color.multiplyAlpha(c, 0.3))
+  love.graphics.circle('line', p.x, p.y, 9)
+end
+
+local labelOffsets = {
+  top = function(textW, textH)
+    return Vec2(-textW/2, -30)
+  end
+}
+local function renderNodeLabel(nodeId, distScale, labelFont)
+  love.graphics.setColor(1,1,1)
+  local ref = Node:get(nodeId)
+  local label = ref.level
+  if label then
+    local humanizedLabel = string.gsub(label, '_', ' ')
+    local textW, textH = getTextSize(humanizedLabel, labelFont)
+    local p = (ref.position * distScale) + labelOffsets[ref.labelPosition](textW, textH)
+    love.graphics.print(humanizedLabel, p.x, p.y)
+  end
+end
+
+local function renderRegionLabel(position, distScale, label, labelFont)
+  love.graphics.setColor(1,1,1)
+  local p = position * distScale
+  love.graphics.print(label, p.x, p.y)
 end
 
 local function renderLink(node1, node2, distScale)
@@ -82,9 +118,147 @@ local function renderLink(node1, node2, distScale)
   love.graphics.setColor(
     (ref1.secret or ref2.secret) and
       graphColors.secret or
-      graphColors.default
+      graphColors.link
   )
   love.graphics.line(p1.x, p1.y, p2.x, p2.y)
+end
+
+local createUniverse = function()
+  local model = Node:createModel()
+
+  local level1 = Node:create({
+    position = Vec2(20, 20),
+    region = 'aureus',
+    level = 'floor_1',
+    labelPosition = 'top'
+  })
+
+  local level2 = Node:create({
+    position = Vec2(
+      Node:get(level1).position.x + 40,
+      Node:get(level1).position.y
+    ),
+    region = 'aureus',
+    level = 'floor_2',
+    labelPosition = 'top'
+  })
+  model:addLink(level2, level1)
+
+  local level3 = Node:create({
+    position = Vec2(
+      Node:get(level2).position.x + 50,
+      Node:get(level2).position.y + 20
+    ),
+    region = 'saria'
+  })
+  model:addLink(level3, level2)
+
+  local level4 = Node:create({
+    position = Vec2(
+      Node:get(level3).position.x + 30,
+      Node:get(level3).position.y
+    ),
+    region = 'saria'
+  })
+  model:addLink(level4, level3)
+
+  local level5 = Node:create({
+    position = Vec2(
+      Node:get(level4).position.x + 15,
+      Node:get(level4).position.y + 25
+    ),
+    region = 'saria'
+  })
+  model:addLink(level5, level4)
+
+  local level6 = Node:create({
+    position = Vec2(
+      Node:get(level4).position.x - 15,
+      Node:get(level4).position.y + 25
+    ),
+    region = 'saria'
+  })
+  model:addLink(level6, level4)
+
+  local avgPos = (Node:get(level2).position + Node:get(level3).position) / 2
+  local secretLevel1 = Node:create({
+    position = Vec2(
+      avgPos.x - 15,
+      avgPos.y + 20
+    ),
+    secret = true,
+    region = 'secret'
+  })
+  model:addLink(secretLevel1, level2)
+
+  local avgPos = (Node:get(level2).position + Node:get(level4).position) / 2
+  local secretLevel2 = Node:create({
+    position = Vec2(
+      avgPos.x + 6,
+      avgPos.y - 15
+    ),
+    secret = true,
+    region = 'secret'
+  })
+  model:addLink(secretLevel2, level4)
+
+  return model
+end
+
+local renderCanvas = love.graphics.newCanvas(4096, 4096)
+local renderGraph = function(graph, distScale)
+  local nodesToRender = {}
+  love.graphics.push()
+  love.graphics.origin()
+  local oBlendMode = love.graphics.getBlendMode()
+  love.graphics.setBlendMode('alpha', 'premultiplied')
+  love.graphics.setCanvas(renderCanvas)
+  love.graphics.clear()
+  love.graphics.setLineStyle('rough')
+
+  graph:forEach(function(_, node1, node2)
+    if (not nodesToRender[node1]) then
+      nodesToRender[node1] = node1
+    end
+    if (not nodesToRender[node2]) then
+      nodesToRender[node2] = node2
+    end
+    renderLink(node1, node2, distScale)
+  end)
+
+  love.graphics.setCanvas()
+  love.graphics.pop()
+  love.graphics.setColor(1,1,1)
+  love.graphics.setBlendMode(oBlendMode)
+
+  local lineGapStencil = function()
+    love.graphics.setColor(1,1,1)
+    local clamp = require 'utils.math'.clamp
+    local radius = clamp(14 * distScale/2, 12, 14)
+    for nodeId in pairs(nodesToRender) do
+      local ref = Node:get(nodeId)
+      local p = ref.position * distScale
+      love.graphics.circle('fill', p.x, p.y, radius)
+    end
+  end
+  love.graphics.stencil(lineGapStencil, 'replace', 1)
+
+  love.graphics.setStencilTest('notequal', 1)
+  love.graphics.draw(renderCanvas)
+  love.graphics.setStencilTest()
+
+  love.graphics.setLineWidth(1)
+  for nodeId in pairs(nodesToRender) do
+    renderNode(nodeId, distScale)
+  end
+
+  local oFont = love.graphics.getFont()
+  local labelFont = Font.secondary.font
+  love.graphics.setFont(labelFont)
+  for nodeId in pairs(nodesToRender) do
+    renderNodeLabel(nodeId, distScale, labelFont)
+  end
+  love.graphics.setFont(oFont)
 end
 
 Component.create({
@@ -98,114 +272,25 @@ Component.create({
     end
 
     local homeScreenRef = Component.get('HomeScreen')
-    if homeScreenRef then
+  if homeScreenRef then
       homeScreenRef:delete(true)
     end
 
-    local model = Node:createModel()
-
-    local level1 = Node:create({
-      position = Vec2(),
-      world = 'aureus'
-    })
-
-    local level2 = Node:create({
-      position = Vec2(
-        Node:get(level1).position.x + 30,
-        Node:get(level1).position.y
-      ),
-      world = 'aureus'
-    })
-    model:addLink(level2, level1)
-
-    local level3 = Node:create({
-      position = Vec2(
-        Node:get(level2).position.x + 60,
-        Node:get(level2).position.y + 30
-      )
-    })
-    model:addLink(level3, level2)
-
-    local level4 = Node:create({
-      position = Vec2(
-        Node:get(level3).position.x + 30,
-        Node:get(level3).position.y
-      )
-    })
-    model:addLink(level4, level3)
-
-    local level5 = Node:create({
-      position = Vec2(
-        Node:get(level4).position.x + 15,
-        Node:get(level4).position.y + 25
-      )
-    })
-    model:addLink(level5, level4)
-
-    local level6 = Node:create({
-      position = Vec2(
-        Node:get(level4).position.x - 15,
-        Node:get(level4).position.y + 25
-      )
-    })
-    model:addLink(level6, level4)
-
-    local avgPos = (Node:get(level2).position + Node:get(level3).position) / 2
-    local secretLevel1 = Node:create({
-      position = Vec2(
-        avgPos.x - 15,
-        avgPos.y + 40
-      ),
-      secret = true
-    })
-    model:addLink(secretLevel1, level2)
-    model:addLink(secretLevel1, level3)
-
-    local renderCanvas = love.graphics.newCanvas(4096, 4096)
-    local distScale = 2 -- the amount to scale the distance between the nodes
-    self.renderGraph = function()
-      local nodesToRender = {}
-      love.graphics.push()
-      love.graphics.origin()
-      local oBlendMode = love.graphics.getBlendMode()
-      love.graphics.setBlendMode('alpha', 'premultiplied')
-      love.graphics.setCanvas(renderCanvas)
-      love.graphics.clear()
-      love.graphics.setLineStyle('rough')
-
-      model:forEach(function(_, node1, node2)
-        if (not nodesToRender[node1]) then
-          nodesToRender[node1] = node1
-        end
-        if (not nodesToRender[node2]) then
-          nodesToRender[node2] = node2
-        end
-        renderLink(node1, node2, distScale)
-      end)
-
-      love.graphics.setCanvas()
-      love.graphics.pop()
-      love.graphics.setColor(1,1,1)
-      love.graphics.setBlendMode(oBlendMode)
-
-      love.graphics.stencil(function()
-        love.graphics.setColor(1,1,1)
-        for nodeId in pairs(nodesToRender) do
-          local ref = Node:get(nodeId)
-          local p = ref.position * distScale
-          love.graphics.circle('fill', p.x, p.y, 12)
-        end
-      end, 'replace', 1)
-
-      love.graphics.setStencilTest('notequal', 1)
-      love.graphics.draw(renderCanvas)
-      love.graphics.setStencilTest()
-
-      love.graphics.setLineWidth(1)
-      for nodeId in pairs(nodesToRender) do
-        renderNode(nodeId, distScale)
+    Gui.create({
+      x = 0,
+      y = 0,
+      w = love.graphics.getWidth(),
+      h = love.graphics.getHeight(),
+      onWheel = function(self, ev)
+        local dy = ev[2]
+        local clamp = require 'utils.math'.clamp
+        Component.animate(state, {
+          distScale = clamp(state.distScale + dy, 1, 4)
+        }, 0.25, 'outCubic')
       end
-    end
+    })
+
+    self.graph = createUniverse()
   end,
 
   update = function(self, dt)
@@ -217,7 +302,7 @@ Component.create({
     love.graphics.translate(180, 100)
     love.graphics.scale(2)
 
-    self.renderGraph()
+    renderGraph(self.graph, state.distScale)
 
     love.graphics.pop()
   end
