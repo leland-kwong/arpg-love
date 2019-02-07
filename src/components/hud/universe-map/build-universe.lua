748561e7698f09d45426d35a71faf3e8c886ab50
leland-kwong <leland.kwong@gmail.com>
2019-02-07 00:15:55 -0800

    Hide levels that haven't been created yet

--- src/components/hud/universe-map/build-universe.lua
+++ src/components/hud/universe-map/build-universe.lua
@@ -27,59 +27,59 @@ return function(actions)
   })
   uSystem:newLink('1-2', '1-3')
 
-  uSystem:setNode('2-1', {
-    position = uSystem:getNode('1-3').position + Vec2(50, 20),
-    level = 's-1',
-    region = 'saria'
-  })
-  uSystem:newLink('1-3', '2-1')
+  -- uSystem:setNode('2-1', {
+  --   position = uSystem:getNode('1-3').position + Vec2(50, 20),
+  --   level = 's-1',
+  --   region = 'saria'
+  -- })
+  -- uSystem:newLink('1-3', '2-1')
 
-  uSystem:setNode('2-2', {
-    position = uSystem:getNode('2-1').position + Vec2(30, 0),
-    level = 's-2',
-    region = 'saria'
-  })
-  uSystem:newLink('2-1', '2-2')
+  -- uSystem:setNode('2-2', {
+  --   position = uSystem:getNode('2-1').position + Vec2(30, 0),
+  --   level = 's-2',
+  --   region = 'saria'
+  -- })
+  -- uSystem:newLink('2-1', '2-2')
 
-  uSystem:setNode('2-3', {
-    position = uSystem:getNode('2-2').position + Vec2(15, 25),
-    level = 's-3',
-    labelPosition = 'right',
-    region = 'saria'
-  })
-  uSystem:newLink('2-2', '2-3')
+  -- uSystem:setNode('2-3', {
+  --   position = uSystem:getNode('2-2').position + Vec2(15, 25),
+  --   level = 's-3',
+  --   labelPosition = 'right',
+  --   region = 'saria'
+  -- })
+  -- uSystem:newLink('2-2', '2-3')
 
-  uSystem:setNode('2-4', {
-    position = uSystem:getNode('2-2').position + Vec2(-15, 25),
-    level = 's-4',
-    labelPosition = 'left',
-    region = 'saria'
-  })
-  uSystem:newLink('2-2', '2-4')
+  -- uSystem:setNode('2-4', {
+  --   position = uSystem:getNode('2-2').position + Vec2(-15, 25),
+  --   level = 's-4',
+  --   labelPosition = 'left',
+  --   region = 'saria'
+  -- })
+  -- uSystem:newLink('2-2', '2-4')
 
-  local avgPos = (uSystem:getNode('1-2').position + uSystem:getNode('2-1').position) / 2
-  uSystem:setNode('secret-1', {
-    position = Vec2(
-      avgPos.x - 15,
-      avgPos.y + 20
-    ),
-    level = 'secret-1',
-    secret = true,
-    region = ''
-  })
-  uSystem:newLink('2-4', 'secret-1')
+  -- local avgPos = (uSystem:getNode('1-2').position + uSystem:getNode('2-1').position) / 2
+  -- uSystem:setNode('secret-1', {
+  --   position = Vec2(
+  --     avgPos.x - 15,
+  --     avgPos.y + 20
+  --   ),
+  --   level = 'secret-1',
+  --   secret = true,
+  --   region = ''
+  -- })
+  -- uSystem:newLink('2-4', 'secret-1')
 
-  local avgPos = (uSystem:getNode('1-2').position + uSystem:getNode('2-2').position) / 2
-  uSystem:setNode('secret-2', {
-    position = Vec2(
-      avgPos.x + 5,
-      avgPos.y - 32
-    ),
-    level = 'secret-2',
-    secret = true,
-    region = ''
-  })
-  uSystem:newLink('secret-2', '2-1')
+  -- local avgPos = (uSystem:getNode('1-2').position + uSystem:getNode('2-2').position) / 2
+  -- uSystem:setNode('secret-2', {
+  --   position = Vec2(
+  --     avgPos.x + 5,
+  --     avgPos.y - 32
+  --   ),
+  --   level = 'secret-2',
+  --   secret = true,
+  --   region = ''
+  -- })
+  -- uSystem:newLink('secret-2', '2-1')
 
   actions.newGraph('universe')
 
