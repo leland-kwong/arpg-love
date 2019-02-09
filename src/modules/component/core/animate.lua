1a9e14a26897c647d1dfea14dab5362e975140b1
leland-kwong <leland.kwong@gmail.com>
2019-02-08 20:16:13 -0800

    Add support for update callback

--- src/modules/component/core/animate.lua
+++ src/modules/component/core/animate.lua
@@ -4,10 +4,11 @@ local noop = require 'utils.noop'
 return function(Component)
   local tweens = {}
 
-  Component.animate = function (subject, target, duration, easing, onComplete)
+  Component.animate = function (subject, target, duration, easing, onUpdate, onComplete)
     local tween = tween.new(duration, subject, target, easing)
     tweens[subject] = {
       tween = tween,
+      onUpdate = onUpdate,
       onComplete = onComplete or noop
     }
     return tween
@@ -16,6 +17,9 @@ return function(Component)
   Component.animateUpdate = function(dt)
     for ref,t in pairs(tweens) do
       local complete = t.tween:update(dt)
+      if t.onUpdate then
+        t.onUpdate(dt)
+      end
       if complete then
         t.onComplete()
         tweens[ref] = nil
