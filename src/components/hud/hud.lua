903c92b8263a10b917a7b5e5798511428048b3f9
leland-kwong <leland.kwong@gmail.com>
2019-02-10 14:35:58 -0800

    [zone-info] use a unique id to prevent duplicates

--- src/components/hud/hud.lua
+++ src/components/hud/hud.lua
@@ -287,7 +287,9 @@ function Hud.init(self)
     end),
     msgBus.on(msgBus.SCENE_CHANGE, function(sceneRef)
       local ZoneInfo = require 'components.hud.zone-info'
-      ZoneInfo.create()
+      ZoneInfo.create({
+        id = 'ZoneInfo'
+      })
     end)
   }
 
