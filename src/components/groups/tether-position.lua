b9c6ae186f4c6091e0e4f6f37db4ccf35e82752d
leland-kwong <leland.kwong@gmail.com>
2019-02-14 14:45:51 -0800

    Do position updates at the end of update cycle

--- src/components/groups/tether-position.lua
+++ src/components/groups/tether-position.lua
@@ -1,47 +1,53 @@
 local Component = require 'modules.component'
+local msgBus = require 'components.msg-bus'
 
 Component.create({
   id = 'TetherPositionSystem',
   init = function(self)
-    Component.addToGroup(self, 'firstLayer')
-  end,
-  update = function(self, dt)
-    local components = Component.groups.tetherPosition.getAll()
+    self.listeners = {
+      msgBus.on('UPDATE_END', function()
+        local components = Component.groups.tetherPosition.getAll()
 
-    local tetherParentsToUpdate = {}
+        local tetherParentsToUpdate = {}
 
-    for _,child in pairs(components) do
-      local tp = child._tetherParent
-      if (not tp) then
-        Component.removeFromGroup('tetherPosition', child)
-      else
-        local tpx,tpy,tpz = tp.x, tp.y, tp.z
-        local hasChangedPosition = tpx ~= tp._prevX
-          or tpy ~= tp._prevY
-          or tpz ~= tp._prevZ
+        for _,child in pairs(components) do
+          local tp = child._tetherParent
+          if (not tp) then
+            Component.removeFromGroup('tetherPosition', child)
+          else
+            local tpx,tpy,tpz = tp.x, tp.y, tp.z
+            local hasChangedPosition = tpx ~= tp._prevX
+              or tpy ~= tp._prevY
+              or tpz ~= tp._prevZ
+              or (not child.prevParentX)
 
-        if hasChangedPosition then
-          table.insert(tetherParentsToUpdate, tp)
+            if hasChangedPosition then
+              table.insert(tetherParentsToUpdate, tp)
 
-          -- update position relative to its parent
-          local dx, dy, dz =
-            (tpx - (child.prevParentX or tpx)),
-            (tpy - (child.prevParentY or tpy)),
-            (tpz - (child.prevParentZ or tpz))
-          child:setPosition(child.x + dx, child.y + dy, child.z + dz)
-          child.prevParentX = tpx
-          child.prevParentY = tpy
-          child.prevParentZ = tpz
+              -- update position relative to its parent
+              local dx, dy, dz =
+                (tpx - (child.prevParentX or tpx)),
+                (tpy - (child.prevParentY or tpy)),
+                (tpz - (child.prevParentZ or tpz))
+              child:setPosition(child.x + dx, child.y + dy, child.z + dz)
+              child.prevParentX = tpx
+              child.prevParentY = tpy
+              child.prevParentZ = tpz
+            end
+          end
         end
-      end
-    end
 
-    for i=1, #tetherParentsToUpdate do
-      local tp = tetherParentsToUpdate[i]
-      tp._prevX = tp.x
-      tp._prevY = tp.y
-      tp._prevZ = tp.z
-    end
+        for i=1, #tetherParentsToUpdate do
+          local tp = tetherParentsToUpdate[i]
+          tp._prevX = tp.x
+          tp._prevY = tp.y
+          tp._prevZ = tp.z
+        end
+      end)
+    }
+  end,
+  final = function(self)
+    msgBus.off(self.listeners)
   end
 })
 
