8f7c73835a0398428ad3416d5cd4e4022649b1e1
leland-kwong <leland.kwong@gmail.com>
2019-02-14 01:55:44 -0800

    Tweak spark particle effect

--- src/components/abilities/spark.lua
+++ src/components/abilities/spark.lua
@@ -8,7 +8,7 @@ local Color = require 'modules.color'
 
 local Spark = {
   color = Color.WHITE,
-  maxLifeTime = 0.30,
+  maxLifeTime = 0.60,
   opacity = 1
 }
 
@@ -21,25 +21,27 @@ psystem:setOffset(animation:getOffset())
 -- psystem:setSpread(-math.pi * 2, math.pi * 2)
 local speed = 10
 psystem:setSpeed(0, speed)
-local acceleration = 60
+local acceleration = 100
 psystem:setLinearAcceleration(0, 0, 0, acceleration) -- move particles in all random directions
 psystem:setSizes(2, 3)
+psystem:setRotation(math.pi/4, math.pi/4)
 psystem:setSizeVariation(1)
-psystem:setEmissionArea('ellipse', 10, 10, 0, true)
+psystem:setEmissionArea('ellipse', 7, 7, 0, true)
 
 -- particle system
 local ParticleSystem = {}
 
 function ParticleSystem.init(self)
   Component.addToGroup(self, 'all')
+  self.psystem = psystem:clone()
 end
 
 function ParticleSystem.update(self, dt)
-  psystem:update(dt)
+  self.psystem:update(dt)
 end
 
 function ParticleSystem.draw(self)
-  love.graphics.draw(psystem, 0, 0)
+  love.graphics.draw(self.psystem, 0, 0)
 end
 
 function ParticleSystem.drawOrder()
@@ -47,21 +49,23 @@ function ParticleSystem.drawOrder()
   return drawOrders.SparkDraw
 end
 
-Component.create(ParticleSystem)
+local Psystem = Component.createFactory(ParticleSystem)
 
 function Spark.init(self)
   Component.addToGroup(self, 'all')
   Component.addToGroup(self, 'gameWorld')
 
-  psystem:setParticleLifetime(0.30, self.maxLifeTime)
+  local ps = Psystem.create():setParent(self)
+  local psystem = ps.psystem
+  psystem:setParticleLifetime(self.maxLifeTime, self.maxLifeTime)
   local col = self.color
   psystem:setColors(
     1, 1, 1, 1,
-    col[1], col[2], col[3], 0.5,
+    col[1], col[2], col[3], 1,
     col[1], col[2], col[3], 0
   )
   psystem:setPosition(self.x, self.y)
-  psystem:emit(20)
+  psystem:emit(10)
 
   self.clock = 0
 end
