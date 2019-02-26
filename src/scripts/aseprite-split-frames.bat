30a18a32425486ee73191ff714349cc8f6696a61
leland-kwong <leland.kwong@gmail.com>
2019-02-25 17:27:07 -0800

    Update aseprite build scripts

--- src/scripts/aseprite-split-frames.bat
+++ src/scripts/aseprite-split-frames.bat
@@ -1,1 +1,13 @@
-"C:\Program Files\Aseprite\Aseprite.exe" -b pod-one.aseprite --save-as pod-one-{frame}.png
+SET ProjectDir=C:\Users\lelandkwong\Projects\arpg-love
+SET SourceDir=%ProjectDir%\assets\sprites\custom-art\characters\player
+SET AsepritePath="C:\Program Files\Aseprite\Aseprite.exe"
+
+DEL %SourceDir%\"*.png"
+%AsepritePath% -b %SourceDir%\player.aseprite --save-as %SourceDir%\{tag}-{frame}.png
+PAUSE
+REM %AsepritePath% -b %SourceDir%\room.aseprite --scale 1 --save-as %SourceDir%\{slice}.png
+REM %AsepritePath% -b %SourceDir%\cursors.aseprite --scale 2 --save-as %SourceDir%\{slice}-2x.png
+REM %AsepritePath% -b %SourceDir%\cursors.aseprite --scale 3 --save-as %SourceDir%\{slice}-3x.png
+
+REM Build sprite sheet
+CALL "%ProjectDir%\src\scripts\texture-packer.bat"
