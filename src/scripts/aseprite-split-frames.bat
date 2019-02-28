a9ebb8b77f7adf72961909e14df9536d0fbe04f1
leland-kwong <leland.kwong@gmail.com>
2019-02-27 16:16:43 -0800

    Remove unused code

--- src/scripts/aseprite-split-frames.bat
+++ src/scripts/aseprite-split-frames.bat
@@ -4,10 +4,6 @@ SET AsepritePath="C:\Program Files\Aseprite\Aseprite.exe"
 
 DEL %SourceDir%\"*.png"
 %AsepritePath% -b %SourceDir%\player.aseprite --save-as %SourceDir%\{tag}-{frame}.png
-PAUSE
-REM %AsepritePath% -b %SourceDir%\room.aseprite --scale 1 --save-as %SourceDir%\{slice}.png
-REM %AsepritePath% -b %SourceDir%\cursors.aseprite --scale 2 --save-as %SourceDir%\{slice}-2x.png
-REM %AsepritePath% -b %SourceDir%\cursors.aseprite --scale 3 --save-as %SourceDir%\{slice}-3x.png
 
 REM Build sprite sheet
 CALL "%ProjectDir%\src\scripts\texture-packer.bat"
